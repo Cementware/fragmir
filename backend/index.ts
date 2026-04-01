5ce3897abe4ad3061307bfc7cfaa0603197297c1
GamingGuy003 <bjornfelixh@gmail.com>
2026-04-01 14:18:55 +0200

    added mariadb

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,21 @@
+import process = require("node:process");
+
 const express = require('express');
 const cors = require('cors');
+const mariadb = require('mariadb');
 
 const app = express();
 app.use(cors());
+
+console.log('Starting...');
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  connectionLimit: 5
+});
+
+pool.getConnection().then(conn => console.log(conn));
 
 app.get('/api/status', (_, res) => {
   res.json({ message: "Backend is running and connected!" });
